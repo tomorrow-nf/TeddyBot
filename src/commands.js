7d004d56bcdbb58534da227b281eb2529a0218d7
Dylan Kozicki <dylan.kozicki@gmail.com>
2020-11-19 15:25:11 -0500

    fixed members

--- src/commands.js
+++ src/commands.js
@@ -7,7 +7,9 @@ const fs = require('fs');
 const misc = require('./misc.js');
 const blacklist = require('./blacklist.js');
 
-let userCommandList = JSON.parse(fs.readFileSync('./info/userCommands.json', 'utf8'));
+let userCommandList = JSON.parse(
+  fs.readFileSync('./info/userCommands.json', 'utf8')
+);
 
 //Spambot detection
 let spamlist = JSON.parse(fs.readFileSync('./info/spam.json', 'utf8'));
@@ -16,38 +18,57 @@ let commandPrefix = '!';
 let helpString = ['', ''];
 
 // Hard-coded help strings for innate funtions
-helpString[0] += '`!members` - Tell us how many members are on a server, and how many are online.\n';
-helpString[0] += '`!top PLACEMENTS NUM_MESSAGES CHANNEL_NAME` - Tells you the top `PLACEMENTS` most frequent posters over the last `NUM_MESSAGES` messages in #`CHANNEL_NAME` (more messages = more time)\n';
-helpString[0] += '`!purge CHANNEL NUMBER` - Delete NUMBER messages from CHANNEL.\n';
-helpString[0] += '`!setcommand COMMAND_NAME text` - Will create a user-accessible =`COMMAND_NAME` that will make the bot return any text after `COMMAND_NAME`.\n';
-helpString[0] += '`!describecommand COMMAND_NAME description` - Adds a description to display in `!help` for the users.\n';
-helpString[0] += '`!removecommand COMMAND_NAME` - Will remove the user-accessible =`COMMAND_NAME`, if it exists.\n';
-helpString[0] += '`!hidecommand COMMAND_NAME` - Toggles visibility of a help command.\n';
-helpString[0] += '`!helpcount` - Show number of uses each user command has recieved.\n';
+helpString[0] +=
+  '`!members` - Tell us how many members are on a server, and how many are online.\n';
+helpString[0] +=
+  '`!top PLACEMENTS NUM_MESSAGES CHANNEL_NAME` - Tells you the top `PLACEMENTS` most frequent posters over the last `NUM_MESSAGES` messages in #`CHANNEL_NAME` (more messages = more time)\n';
+helpString[0] +=
+  '`!purge CHANNEL NUMBER` - Delete NUMBER messages from CHANNEL.\n';
+helpString[0] +=
+  '`!setcommand COMMAND_NAME text` - Will create a user-accessible =`COMMAND_NAME` that will make the bot return any text after `COMMAND_NAME`.\n';
+helpString[0] +=
+  '`!describecommand COMMAND_NAME description` - Adds a description to display in `!help` for the users.\n';
+helpString[0] +=
+  '`!removecommand COMMAND_NAME` - Will remove the user-accessible =`COMMAND_NAME`, if it exists.\n';
+helpString[0] +=
+  '`!hidecommand COMMAND_NAME` - Toggles visibility of a help command.\n';
+helpString[0] +=
+  '`!helpcount` - Show number of uses each user command has recieved.\n';
 helpString[0] += '`!helphidden` - Display hidden user commands.\n';
-helpString[0] += '`!kill` - End this bot instance. Bot should automatically restart.\n';
+helpString[0] +=
+  '`!kill` - End this bot instance. Bot should automatically restart.\n';
 helpString[0] += '`!say CHANNEL MESSAGE` - Send any message to any channel.\n';
 
-helpString[1] += '`!emotelist EMOTES` - The list of emotes to add to a message when reacting with a setup react.\n';
+helpString[1] +=
+  '`!emotelist EMOTES` - The list of emotes to add to a message when reacting with a setup react.\n';
 helpString[1] += '`!blacklist` - List all words currently on the blacklist.\n';
 helpString[1] += '`!blacklist add word` - Add `word` to the blacklist.\n';
-helpString[1] += '`!blacklist remove word` - Remove `word` from the blacklist.\n';
-helpString[1] += '`!blacklist violations ID|Tag` - List all words that were removed as violations from a user with that ID or Tag.\n';
-helpString[1] += '`!blacklist warnings ID|Tag` - List all words that were flagged as warnings from a user with that ID or Tag.\n';
-helpString[1] += '`!log` - Print a log of all users with recorded blacklist warnings or infractions.\n';
-helpString[1] += '`!logfile` - Send a .csv file containing users and the quantity of violations/warnings.\n';
-helpString[1] += '`!spambots add word` - Add a string to the new user spambot filter\n';
-helpString[1] += '`!spambots remove word` - Remove a string from the spambot filter\n';
+helpString[1] +=
+  '`!blacklist remove word` - Remove `word` from the blacklist.\n';
+helpString[1] +=
+  '`!blacklist violations ID|Tag` - List all words that were removed as violations from a user with that ID or Tag.\n';
+helpString[1] +=
+  '`!blacklist warnings ID|Tag` - List all words that were flagged as warnings from a user with that ID or Tag.\n';
+helpString[1] +=
+  '`!log` - Print a log of all users with recorded blacklist warnings or infractions.\n';
+helpString[1] +=
+  '`!logfile` - Send a .csv file containing users and the quantity of violations/warnings.\n';
+helpString[1] +=
+  '`!spambots add word` - Add a string to the new user spambot filter\n';
+helpString[1] +=
+  '`!spambots remove word` - Remove a string from the spambot filter\n';
 
 async function modCommands(message, args) {
   if (args[0] == '!members') {
-    let memberList = message.guild.members.array();
+    let memberList = message.guild.members.cache.array();
     let memberCount = message.guild.memberCount;
     let onlineCount = 0;
     for (let i = 0; i < memberList.length; i++) {
       if (memberList[i].presence.status != 'offline') onlineCount += 1;
     }
-    return await message.channel.send(`${message.guild.name} currently has ${memberCount} total members. ${onlineCount} members are currently online.`);
+    return await message.channel.send(
+      `${message.guild.name} currently has ${memberCount} total members. ${onlineCount} members are currently online.`
+    );
   } else if (args[0] == '!top') {
     if (args.length < 4) {
       return await message.channel.send(
@@ -99,13 +120,14 @@ async function modCommands(message, args) {
     dictArray.sort(function (a, b) {
       return b[1] - a[1];
     });
-    let str = `Top ${placements} #${args[3]} posters as of the last ${args[2]} messages, since ${msgArray[msgArray.length - 1].createdAt} :\n\n`;
+    let str = `Top ${placements} #${args[3]} posters as of the last ${
+      args[2]
+    } messages, since ${msgArray[msgArray.length - 1].createdAt} :\n\n`;
     for (let i = 0; i < placements && i < dictArray.length; i++) {
       str += dictArray[i][2] + ': ' + dictArray[i][1] + '\n';
     }
     console.log(str);
     return await resultsMessage.edit(str);
-
   } else if (args[0] == '!purge') {
     if (args.length < 3)
       return await message.channel.send(
@@ -117,13 +139,23 @@ async function modCommands(message, args) {
     try {
       if (args[1].startsWith('#')) args[1] = args[1].substring(1);
       quantity_messages = parseInt(args[2]);
-      relevant_channel = message.guild.channels.cache.find(channel => channel.name === args[1]);
-      console.log("Received purge request for channel " + relevant_channel.name + ", " + quantity_messages + " messages");
+      relevant_channel = message.guild.channels.cache.find(
+        (channel) => channel.name === args[1]
+      );
+      console.log(
+        'Received purge request for channel ' +
+          relevant_channel.name +
+          ', ' +
+          quantity_messages +
+          ' messages'
+      );
     } catch (e) {
       return await message.channel.send(e.message);
     }
     if (relevant_channel == null) {
-      return await message.channel.send('Could not find a channel with that name.');
+      return await message.channel.send(
+        'Could not find a channel with that name.'
+      );
     }
     let mChannel = message.channel;
     await message.delete();
@@ -137,8 +169,9 @@ async function modCommands(message, args) {
         await msgs[j].delete();
       }
     }
-    return await mChannel.send(quantity_messages.toString() + ' messages deleted from ' + args[1] + '.');
-
+    return await mChannel.send(
+      quantity_messages.toString() + ' messages deleted from ' + args[1] + '.'
+    );
   } else if (args[0] == '!modhelp') {
     let s = 'Here are some commands available for moderators:\n\n';
     if (args.length == 1) s += helpString[0];
@@ -155,15 +188,15 @@ async function modCommands(message, args) {
         },
       ],
     });
-
   } else if (args[0] == '!kill') {
     console.log('Received !kill command, restarting bot');
     await message.channel.send('Received !kill command, restarting bot');
     process.exit(1);
-
   } else if (args[0] == '!say') {
     if (args.length < 3) {
-      return await message.channel.send('USAGE: `!say CHANNEL MESSAGE` -- example: `!say general Hello world!`');
+      return await message.channel.send(
+        'USAGE: `!say CHANNEL MESSAGE` -- example: `!say general Hello world!`'
+      );
     }
     let len = args[0].length + args[1].length + 2;
     if (args[1].startsWith('#')) {
@@ -171,7 +204,9 @@ async function modCommands(message, args) {
     }
     let relevant_channel = null;
     try {
-      relevant_channel = message.guild.channels.cache.find(channel => channel.name === args[1]);
+      relevant_channel = message.guild.channels.cache.find(
+        (channel) => channel.name === args[1]
+      );
     } catch (e) {
       return await message.channel.send(e.message);
     }
@@ -181,7 +216,6 @@ async function modCommands(message, args) {
       );
     }
     return await relevant_channel.send(message.content.substring(len));
-
   } else if (args[0] == '!setcommand' || args[0] == '!addcommand') {
     if (args.length < 3) {
       return await message.channel.send(
@@ -193,7 +227,9 @@ async function modCommands(message, args) {
       for (let i = 0; i < userCommandList.length; i++) {
         if (userCommandList[i].command == commandPrefix + args[1]) {
           //just update its text
-          userCommandList[i].text = message.content.substring(args[0].length + args[1].length + 2);
+          userCommandList[i].text = message.content.substring(
+            args[0].length + args[1].length + 2
+          );
           exists = true;
         }
       }
@@ -208,14 +244,21 @@ async function modCommands(message, args) {
         userCommandList.push(toAdd);
       }
 
-      fs.writeFileSync('./info/userCommands.json',JSON.stringify(userCommandList, null, '\t'),'utf8');
+      fs.writeFileSync(
+        './info/userCommands.json',
+        JSON.stringify(userCommandList, null, '\t'),
+        'utf8'
+      );
       let s = exists ? 'Modified ' : 'Created ';
       return await message.channel.send(
         s + 'the `' + commandPrefix + args[1] + '` command.'
       );
     }
-
-  } else if (args[0] == '!removecommand' || args[0] == '!deletecommand' || args[0] == '!delcommand') {
+  } else if (
+    args[0] == '!removecommand' ||
+    args[0] == '!deletecommand' ||
+    args[0] == '!delcommand'
+  ) {
     if (args.length < 2) {
       return await message.channel.send(
         'USAGE: `!removecommand COMMAND_NAME` - For example `!removecommand controllers` would remove the `!controllers` command.'
@@ -228,9 +271,14 @@ async function modCommands(message, args) {
       }
     }
 
-    fs.writeFileSync('./info/userCommands.json',JSON.stringify(userCommandList, null, '\t'),'utf8');
-    return await message.channel.send('Removed `' + commandPrefix + args[1] + '`.');
-
+    fs.writeFileSync(
+      './info/userCommands.json',
+      JSON.stringify(userCommandList, null, '\t'),
+      'utf8'
+    );
+    return await message.channel.send(
+      'Removed `' + commandPrefix + args[1] + '`.'
+    );
   } else if (args[0] == '!describecommand') {
     if (args.length < 3) {
       return await message.channel.send(
@@ -255,14 +303,19 @@ async function modCommands(message, args) {
         JSON.stringify(userCommandList, null, '\t'),
         'utf8'
       );
-      return await message.channel.send('Updated description of `' + commandPrefix + args[1] + '`.');
+      return await message.channel.send(
+        'Updated description of `' + commandPrefix + args[1] + '`.'
+      );
     } else {
-      return await message.channel.send('Could not find `' + commandPrefix + args[1] + '`.');
+      return await message.channel.send(
+        'Could not find `' + commandPrefix + args[1] + '`.'
+      );
     }
-
   } else if (args[0] == '!hidecommand') {
     if (args.length < 2)
-      return await message.channel.send('USAGE: `!hidecommand COMMANDNAME` ie to hide the `!ping` command type `!hidecommand ping`.');
+      return await message.channel.send(
+        'USAGE: `!hidecommand COMMANDNAME` ie to hide the `!ping` command type `!hidecommand ping`.'
+      );
     if (args[1][0] == commandPrefix)
       //remove user-typed prefix if it exists
       args[1] = args[1].substring(1);
@@ -273,19 +326,35 @@ async function modCommands(message, args) {
     if (index != -1) {
       if (!userCommandList[index].hide) userCommandList[index].hide = true;
       else userCommandList[index].hide = false;
-      fs.writeFileSync('./info/userCommands.json',JSON.stringify(userCommandList, null, '\t'),'utf8');
-      return await message.channel.send('Set `' + commandPrefix + args[1] +'` to ' + (userCommandList[index].hide ? 'hidden' : 'visible') + '.');
+      fs.writeFileSync(
+        './info/userCommands.json',
+        JSON.stringify(userCommandList, null, '\t'),
+        'utf8'
+      );
+      return await message.channel.send(
+        'Set `' +
+          commandPrefix +
+          args[1] +
+          '` to ' +
+          (userCommandList[index].hide ? 'hidden' : 'visible') +
+          '.'
+      );
     } else {
-      return await message.channel.send('Could not find the command `' + commandPrefix + args[1] + '`.');
+      return await message.channel.send(
+        'Could not find the command `' + commandPrefix + args[1] + '`.'
+      );
     }
-
   } else if (args[0] == '!helpcount') {
     let s = 'Command usage stats:\n';
     for (let i = 0; i < userCommandList.length; i++) {
-      s += '`' + userCommandList[i].command + '`: ' + (userCommandList[i].count ? userCommandList[i].count.toString() : '0') + '\n';
+      s +=
+        '`' +
+        userCommandList[i].command +
+        '`: ' +
+        (userCommandList[i].count ? userCommandList[i].count.toString() : '0') +
+        '\n';
     }
     return await message.channel.send(s);
-
   } else if (args[0] == '!helphidden') {
     let s = 'Hidden help commands:\n';
     for (let i = 0; i < userCommandList.length; i++) {
@@ -295,7 +364,7 @@ async function modCommands(message, args) {
     }
     return await message.channel.send(s);
 
-  // Spambot list control commands
+    // Spambot list control commands
   } else if (args[0] == '!spambots') {
     if (args.length == 1) {
       let str = 'Words on the spambot filter: \n`';
@@ -304,7 +373,6 @@ async function modCommands(message, args) {
       }
       str += '`';
       return await message.channel.send(str);
-
     } else if (args.length > 1 && args[1] == 'add') {
       if (args.length > 2) {
         spamlist.push(args[2]);
@@ -320,30 +388,45 @@ async function modCommands(message, args) {
         let ind = spamlist.indexOf(args[2]);
         if (ind > -1) {
           spamlist.splice(ind, 1);
-          fs.writeFileSync('./info/spam.json', JSON.stringify(spamlist),'utf8');
-          await message.channel.send('`' + args[2] + '` has been removed from the spamlist.');
+          fs.writeFileSync(
+            './info/spam.json',
+            JSON.stringify(spamlist),
+            'utf8'
+          );
+          await message.channel.send(
+            '`' + args[2] + '` has been removed from the spamlist.'
+          );
         } else {
-          await message.channel.send('`' + args[2] + '` was not found in the spamlist.');
+          await message.channel.send(
+            '`' + args[2] + '` was not found in the spamlist.'
+          );
         }
       } else {
         await message.channel.send('Usage: `!spamlist remove word`');
       }
     }
-  } else if (args[0] === "!meme"){
-    if (misc.ids.memesChannel != '0' &&  message.channel.id == misc.ids.memesChannel){
+  } else if (args[0] === '!meme') {
+    if (
+      misc.ids.memesChannel != '0' &&
+      message.channel.id == misc.ids.memesChannel
+    ) {
       fs.readdir('./img/memes/', (err, files) => {
         var num;
-        if (args.length === 2){
+        if (args.length === 2) {
           num = args[1];
-        } else{
+        } else {
           num = Math.floor(Math.random() * files.length);
         }
-        console.log("Fetching meme #" + num + ", Number of files: " + files.length);
+        console.log(
+          'Fetching meme #' + num + ', Number of files: ' + files.length
+        );
         return message.channel.send({
-          files: [{
-            attachment: "./img/memes/meme" + num +".png",
-            name: "meme" + num +".png"
-          }]
+          files: [
+            {
+              attachment: './img/memes/meme' + num + '.png',
+              name: 'meme' + num + '.png',
+            },
+          ],
         });
       });
     }
@@ -355,21 +438,32 @@ async function userCommands(message, args) {
     let userHelpString = '';
     for (let i = 0; i < userCommandList.length; i++) {
       if (!userCommandList[i].hide) {
-        userHelpString += '`' + userCommandList[i].command + '` -  ' + userCommandList[i].description + '\n';
+        userHelpString +=
+          '`' +
+          userCommandList[i].command +
+          '` -  ' +
+          userCommandList[i].description +
+          '\n';
       }
     }
-    if (misc.ids.memesChannel != '0'){
-      userHelpString += '`!meme` - Post a random meme (only useable in the memes channel).\n';
+    if (misc.ids.memesChannel != '0') {
+      userHelpString +=
+        '`!meme` - Post a random meme (only useable in the memes channel).\n';
     }
-    return await message.channel.send(`Here's a list of commands for all users:\n${userHelpString}`);
-
+    return await message.channel.send(
+      `Here's a list of commands for all users:\n${userHelpString}`
+    );
   } else if (args[0].startsWith(commandPrefix)) {
     for (let i = 0; i < userCommandList.length; i++) {
       //check through all defined userCommands
       if (args[0] == userCommandList[i].command) {
         if (!userCommandList[i].count) userCommandList[i].count = 0;
         userCommandList[i].count += 1;
-        fs.writeFileSync('./info/userCommands.json',JSON.stringify(userCommandList, null, '\t'),'utf8');
+        fs.writeFileSync(
+          './info/userCommands.json',
+          JSON.stringify(userCommandList, null, '\t'),
+          'utf8'
+        );
         return await message.channel.send(userCommandList[i].text);
       }
     }
